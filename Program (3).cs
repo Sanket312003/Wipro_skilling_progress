4a5b576c43bc2c9600eb808ddc4f00ee388eec3c
Sanket312003 <sanketdakare1@gmail.com>
2026-04-28 00:17:49 +0530

    Add files via upload

---
+++ Program (3).cs
@@ -0,0 +1,61 @@
+﻿using Day4_Assignment;
+
+class Program
+{
+    static void Main()
+    {
+        TransactionService service = new TransactionService();
+
+        while (true)
+        {
+            Console.WriteLine("\n1. Add Account");
+            Console.WriteLine("2. Add Transaction");
+            Console.WriteLine("3. Process Transaction");
+            Console.WriteLine("4. Rollback Last");
+            Console.WriteLine("5. View Balances");
+            Console.WriteLine("6. View History");
+            Console.WriteLine("7. Exit");
+
+            Console.Write("Choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Account ID: ");
+                    string acc = Console.ReadLine();
+                    Console.Write("Initial Balance: ");
+                    double bal = double.Parse(Console.ReadLine());
+                    service.AddAccount(acc, bal);
+                    break;
+
+                case 2:
+                    service.AddTransaction();
+                    break;
+
+                case 3:
+                    service.ProcessTransaction();
+                    break;
+
+                case 4:
+                    service.Rollback();
+                    break;
+
+                case 5:
+                    service.ViewBalances();
+                    break;
+
+                case 6:
+                    service.ViewHistory();
+                    break;
+
+                case 7:
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day4_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            SocialMediaService service = new SocialMediaService();
+
+            while (true)
+            {
+                Console.WriteLine("\n1. Add User");
+                Console.WriteLine("2. Add Post");
+                Console.WriteLine("3. Like Post");
+                Console.WriteLine("4. View Posts");
+                Console.WriteLine("5. Undo Action");
+                Console.WriteLine("6. Process Notification");
+                Console.WriteLine("7. View Users");
+                Console.WriteLine("8. Exit");
+
+                Console.Write("Choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter User ID: ");
+                        int userId = int.Parse(Console.ReadLine());
+                        service.AddUser(userId);
+                        break;
+
+                    case 2:
+                        service.AddPost();
+                        break;
+
+                    case 3:
+                        service.LikePost();
+                        break;
+
+                    case 4:
+                        service.ViewPosts();
+                        break;
+
+                    case 5:
+                        service.UndoAction();
+                        break;
+
+                    case 6:
+                        service.ProcessNotification();
+                        break;
+
+                    case 7:
+                        service.ViewUsers();
+                        break;
+
+                    case 8:
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+        }
+    }
+}
