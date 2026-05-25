0af1139506652a338d4bde97ad379dd291a4e459
Sanket312003 <sanketdakare1@gmail.com>
2026-05-25 14:55:06 +0530

    Add files via upload

---
+++ Day 11/calculatorTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using DemoCalcApp;
+using System;
+
+namespace CalcApp_Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void Add_ReturnsCorrectSum()
+        {
+            Assert.That(calculator.Add(5, 2), Is.EqualTo(7));
+        }
+
+        [Test]
+        public void Subtract_ReturnsCorrectDifference()
+        {
+            Assert.That(calculator.Subtract(5, 2), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Multiply_ReturnsCorrectProduct()
+        {
+            Assert.That(calculator.Multiply(5, 2), Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Divide_ReturnsCorrectQuotient()
+        {
+            Assert.That(calculator.Divide(5, 2), Is.EqualTo(2.5));
+        }
+
+        [Test]
+        public void Divide_ByZero_ThrowsException()
+        {
+            Assert.Throws<DivideByZeroException>(() => calculator.Divide(5, 0));
+        }
+    }
+}
